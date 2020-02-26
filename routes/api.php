02daab244d9a2ed22d14394d6f0a7c07337975c1
tejrajs <tejrajs@efleetpass.com.au>
2020-02-26 23:58:48 +1100

    added api routes

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,13 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/properties/suburb/{suburb}', 'PropertyController@suburb')->name('get-property-analytic-suburb');
+Route::get('/properties/state/{state}', 'PropertyController@state')->name('get-property-analytic-suburb');
+Route::get('/properties/country/{country}', 'PropertyController@country')->name('get-property-analytic-suburb');
+Route::post('/property', 'PropertyController@store')->name('create-property');
+Route::post('/property/analytic', 'AnalyticController@store')->name('create-property-analytic');
+Route::put('/property/analytic/{id}', 'AnalyticController@update')->name('put-property-analytic');
+Route::patch('/property/analytic/{id}', 'AnalyticController@update')->name('patch-property-analytic');
+Route::get('/property/analytic/{id}', 'AnalyticController@index')->name('get-property-analytic');
+Route::get('/property/analytic/suburb/{suburb}', 'AnalyticController@index')->name('get-property-analytic');
